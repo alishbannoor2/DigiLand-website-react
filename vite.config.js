5a9d4120765f66699bb1ba6acbaae3a57372f1f2
Alishba Noor <alishbannoor709@gmail.com>
2026-03-28 21:22:47 +0500

    Alter vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // 👇 Add these lines
+  base: './',              // ensures relative paths for assets
+  build: {
+    outDir: 'dist',        // make sure Vercel looks at dist/
+    assetsDir: 'assets',   // keeps images/css/js organized
+  }
 })
